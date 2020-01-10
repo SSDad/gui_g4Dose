21cf6072d58d685629a1c261ed9744b689ed6c73
Zhen Ji <jizhen04@gmail.com>
2020-01-09 21:55:20 -0600

    save g4 dose mat

--- hMenuItem_LoadG4Dose_Callback.m
+++ hMenuItem_LoadG4Dose_Callback.m
@@ -3,26 +3,44 @@ hFig_main = ancestor(src, 'Figure');
 data_main = guidata(hFig_main);
 ctInfo = data_main.tps.ctInfo;
 
-% dicomDoseFile = readtable('averagedDose_ninefield_1to2_100m.csv');
-% machine = computer;
-% if strcmp(machine, 'MACI64')
-%     rrfd = '/Users/dad/Box Sync/Virtual Linac Project for Halcyon/Ruirui/ResultAnalysis';
-% else
-% %     rrfd = 'C:\Users\zji\Box\Virtual Linac Project for Halcyon\Ruirui\ResultAnalysis';
-% %     rrfd = 'C:\Users\liuru\Box\Virtual Linac Project for Halcyon\Ruirui\ResultAnalysis';
-%    rrfd = 'C:\Users\zhen_\Box Sync\Virtual Linac Project for Halcyon\Ruirui\ResultAnalysis';
-% end
-
 rrfd = fullfile(data_main.ptfd, 'G4Dose');
 
 % read from g4 dose file
 [g4DoseFile, pth] = uigetfile(fullfile(rrfd, '*.csv'));
 ffn = fullfile(pth, g4DoseFile);
-g4csvDose = readtable(ffn);
-g4Dose = table2array(g4csvDose);
-g4DoseCol = g4Dose(:, 4);
-data_main.g4DoseCol = single(g4DoseCol/max(g4DoseCol(:)));
 
+g4DoseFileListFN = fullfile(data_main.matfd, 'g4DoseFileList.mat');
+if exist(g4DoseFileListFN, 'file')
+    load(g4DoseFileListFN);
+    idx = find(contains(g4DoseFileList, ffn));
+    if ~isempty(idx)
+        g4DoseFileFN = fullfile(data_main.matfd, ['g4Dose_', num2str(idx)]);
+        load(g4DoseFileFN);
+    else
+        idx = length(g4DoseFileList)+1;
+        g4DoseFileList(idx) = string(ffn);
+        save(g4DoseFileListFN, 'g4DoseFileList');
+
+        g4csvDose = readtable(ffn);
+        g4Dose = table2array(g4csvDose);
+        g4DoseCol = single(g4Dose(:, 4));
+
+        g4DoseFileFN = fullfile(data_main.matfd, ['g4Dose_', num2str(idx)]); 
+        save(g4DoseFileFN, 'g4DoseCol');
+    end
+else
+    g4DoseFileList(1) = string(ffn);
+    save(g4DoseFileListFN, 'g4DoseFileList');
+
+    g4csvDose = readtable(ffn);
+    g4Dose = table2array(g4csvDose);
+    g4DoseCol = single(g4Dose(:, 4));
+
+    g4DoseFileFN = fullfile(data_main.matfd, 'g4Dose_1'); 
+    save(g4DoseFileFN, 'g4DoseCol');
+end
+    
+data_main.g4DoseCol = g4DoseCol/max(g4DoseCol(:));
 downSampling = 4;
 if contains(g4DoseFile, '1to2')
     downSampling = 2;
