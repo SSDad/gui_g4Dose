5ecb41f2db230f7779ecf61184bcea18be573f94
Zhen Ji <zhen_ji@hotmail.com>
2020-08-05 09:56:57 -0500

    save path info. in Temp folder

--- hMenuItem_LoadCTImages_Callback.m
+++ hMenuItem_LoadCTImages_Callback.m
@@ -3,13 +3,38 @@ function hMenuItem_LoadCTImages_Callback(src, evnt)
 hFig_main = ancestor(src, 'Figure');
 data_main = guidata(hFig_main);
 
-machine = computer;
-if strcmp(machine, 'MACI64')
-    testfd = '/Users/dad/Box Sync/Virtual Linac Project for Halcyon/Ruirui/ClinicalPlan';
+% machine = computer;
+% if strcmp(machine, 'MACI64')
+%     testfd = '/Users/dad/Box Sync/Virtual Linac Project for Halcyon/Ruirui/ClinicalPlan';
+% else
+%      testfd = 'C:\Users\liuru\Box\Virtual Linac Project for Halcyon\Ruirui\ClinicalPlan';
+% end
+% matfd = uigetdir(testfd);
+
+%% load patient folder info
+td = tempdir;
+fd_info = fullfile(td, 'HalcyonMonteCarlo');
+fn_info = fullfile(fd_info, 'info.mat');
+if ~exist(fd_info, 'dir')
+    matfd = uigetdir();
+    if matfd ~=0
+        mkdir(fd_info);
+        PatientPath = fileparts(matfd);
+        save(fn_info, 'PatientPath');
+    end
 else
-     testfd = 'C:\Users\liuru\Box\Virtual Linac Project for Halcyon\Ruirui\ClinicalPlan';
+    if ~exist(fn_info, 'file')
+        matfd = uigetdir();
+        if matfd ~=0
+            PatientPath = fileparts(matfd);
+            save(fn_info, 'PatientPath');
+        end
+    else
+        load(fn_info);
+        matfd = uigetdir(PatientPath);
+    end
 end
-matfd = uigetdir(testfd);
+
 
 if ~isempty(matfd)
     data_main.matfd = matfd;
